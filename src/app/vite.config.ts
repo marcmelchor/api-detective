8045941abf71e9cea7f76b1769a11d973ef2f779
Marc Melchor <marco.a.melchor@gmail.com>
2025-04-09 23:11:39 +0200

    feat: Modify vite.conf.ts to custom bundle files.

--- src/app/vite.config.ts
+++ src/app/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith('.css')) {
+            return 'assets/[name].css';
+          }
+          return 'assets/[name].[ext]';
+        },
+      },
+    },
+  },
+});
